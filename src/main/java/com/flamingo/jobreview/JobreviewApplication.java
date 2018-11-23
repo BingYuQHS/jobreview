cff6c8cf8d95935d4d4daf881081205c3ff1b3bc
秦华山 <1756217895@qq.com>
2018-11-23 11:07:30 +0800

    initial commit

---
+++ src/main/java/com/flamingo/jobreview/JobreviewApplication.java
@@ -0,0 +1,12 @@
+package com.flamingo.jobreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobreviewApplication.class, args);
+	}
+}
