96116fa0d0e2c3f06d152147d604c05149631edb
秦华山 <1756217895@qq.com>
2018-11-23 15:07:59 +0800

    jr-message-初始化消息发送服务

---
+++ jr-message/src/main/java/com/flamingo/jrmessage/JrMessageApplication.java
@@ -0,0 +1,12 @@
+package com.flamingo.jrmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrMessageApplication.class, args);
+	}
+}
