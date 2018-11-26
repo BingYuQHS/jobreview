2b73f52bcbc4faae3b3ad29b20012e189b71a78b
秦华山 <1756217895@qq.com>
2018-11-26 15:44:07 +0800

    jr-message 封装前端返回数据格式

---
+++ jr-message/src/main/java/com/flamingo/jrmessage/util/R.java
@@ -0,0 +1,44 @@
+package com.flamingo.jrmessage.util;
+
+import com.flamingo.jrmessage.model.ResultVO;
+
+/***
+ * 工具类
+ * 封装ResultVO
+ */
+public class R {
+
+    /***
+     * 成功，返回数据
+     * @param object
+     * @return
+     */
+    public static ResultVO ok(Object object) {
+        ResultVO resultVO = new ResultVO();
+        resultVO.setData(object);
+        resultVO.setCode(0);
+        resultVO.setMsg("成功");
+        return resultVO;
+    }
+
+    /***
+     * 成功，不返回数据
+     * @return
+     */
+    public static ResultVO ok() {
+        return ok(null);
+    }
+
+    /***
+     * 失败，返回错误信息
+     * @param code
+     * @param msg
+     * @return
+     */
+    public static ResultVO error(Integer code, String msg) {
+        ResultVO resultVO = new ResultVO();
+        resultVO.setCode(code);
+        resultVO.setMsg(msg);
+        return resultVO;
+    }
+}
