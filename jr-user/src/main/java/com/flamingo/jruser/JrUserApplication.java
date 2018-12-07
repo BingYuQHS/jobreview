64e327a26ebb9f415b2c5a6d8f4f089e8dc4f75b
秦华山 <1756217895@qq.com>
2018-12-07 11:01:54 +0800

    jr-user 暂时关闭服务发现和Feign功能

--- jr-user/src/main/java/com/flamingo/jruser/JrUserApplication.java
+++ jr-user/src/main/java/com/flamingo/jruser/JrUserApplication.java
@@ -3,9 +3,13 @@ package com.flamingo.jruser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
+// @EnableFeignClients
+@ComponentScan(basePackages = {"com.flamingo.jruser.model.entity","com.flamingo.jruser"})
 public class JrUserApplication {
 
 	public static void main(String[] args) {
