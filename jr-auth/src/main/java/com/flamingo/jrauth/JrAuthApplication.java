a705d30630ab0cacfd08c56d35904650e4759d02
秦华山 <1756217895@qq.com>
2018-12-13 19:53:54 +0800

    jr-auth 初始化jr-auth

---
+++ jr-auth/src/main/java/com/flamingo/jrauth/JrAuthApplication.java
@@ -0,0 +1,14 @@
+package com.flamingo.jrauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrAuthApplication.class, args);
+	}
+
+}
+
